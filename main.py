a680e827ae71317043322026ca5e8fee94cac591
Helixo <slowiminedev42@gmail.com>
2026-04-09 20:19:26 +0300

    add

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 
-imgOfCapybara = cv2.imread("./data/capybara840x600.jpg")
+imgOfCapybara = cv2.imread("./data/capybara840x600.jpg") 
+imgCpBlure = cv2.GaussianBlur(imgOfCapybara, (19, 19), 0)
 
-cv2.imshow("Test", imgOfCapybara)
+
+
+cv2.imshow("Test", imgCpBlure)
 cv2.waitKey(0)
