e1ae9aa98b4022c791f9d9b5cbe499f9f7e6ec1d
Helixo <slowiminedev42@gmail.com>
2026-04-16 18:58:24 +0300

    лва

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import cv2
 
 imgOfCapybara = cv2.imread("./data/capybara840x600.jpg") 
 imgCpBlure = cv2.GaussianBlur(imgOfCapybara, (19, 19), 0)
+imgOfCapybara_resize = cv2.resize(imgOfCapybara, (512, 512))
+imgOfCapybara_BW = cv2.cvtColor(imgCpBlure, cv2.COLOR_RGB2GRAY)
 
 
-
-cv2.imshow("Test", imgCpBlure)
+cv2.imshow("Trooll Face", imgOfCapybara_BW)
 cv2.waitKey(0)
